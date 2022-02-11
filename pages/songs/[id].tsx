8a5fbc1d4d77551baf50fb6e07bc2294d96f470d
Robert Fuchs <robert.fuchs@logmein.com>
2022-02-11 16:56:54 +0100

    use server side props to load song data

--- pages/songs/[id].tsx
+++ pages/songs/[id].tsx
@@ -1,20 +1,33 @@
 import { useRouter } from "next/router";
-import { Songs } from "../../lib/song-data";
+import { SongData, Songs } from "../../lib/song-data";
 
 import { Text, Box } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 
-export default function Song() {
-  const router = useRouter();
-  const id = router.query.id as string;
+export interface SongProps {
+  song: SongData;
+}
 
+export default function Song(props: SongProps) {
   return (
     <Box paddingX="5vw" paddingY="2vh">
       <Text as="h1" fontSize="1.5rem" paddingBottom="2vh" textAlign="center">
-        {id.toUpperCase()}
+        {props.song.title.toUpperCase()}
       </Text>
       <Text whiteSpace="pre-line">
-        {Songs.find((s) => s.title.toLowerCase() === id).lyrics}
+        {props.song.lyrics}
       </Text>
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params;
+  const song = Songs.find((s) => s.title.toLowerCase() === (id as string).toLowerCase());
+
+  return {
+    props: {
+      song,
+    }
+  };
+};
