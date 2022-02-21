6bb5c9d77b01b2651328f7905f38df9bdb749821
Robert Fuchs <robert.fuchs@logmein.com>
2022-02-21 11:40:31 +0100

    run sentry wizard

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Text, useBreakpointValue, ResponsiveValue } from '@chakra-ui/react';
 
 interface SongProps {
   title: string;
@@ -6,12 +6,19 @@ interface SongProps {
 }
 
 export const Song: React.FC<SongProps> = (props) => {
+  const textAlign = useBreakpointValue({
+    base: 'left',
+    md: 'center',
+  });
+
   return (
     <>
       <Text as='h1' fontSize='1.5rem' paddingBottom='2vh' textAlign='center'>
         {props.title.toUpperCase()}
       </Text>
-      <Text whiteSpace='pre-line'>{props.lyrics}</Text>
+      <Text whiteSpace='pre-line' textAlign={textAlign}>
+        {props.lyrics}
+      </Text>
     </>
   );
 };
