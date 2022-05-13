d277af9021884b474dcb082774d8f758297e2213
Robert Fuchs <robert.fuchs@futurice.com>
2022-05-13 15:54:19 +0200

    try different GA setup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,27 +1,11 @@
 import { Box } from '@chakra-ui/react';
 import Head from 'next/head';
-import Script from 'next/script';
 
 export const Layout: React.FC<{}> = ({ children }) => {
   return (
     <Box width='100vw' minHeight='100vh' backgroundColor='#F4F4F4'>
       <Head>
         <link rel='icon' href='/crown.svg' />
-
-        {/* Global site tag (gtag.js) - Google Analytics */}
-        <Script
-          src='https://www.googletagmanager.com/gtag/js?id=G-0ESQQS3HTP'
-          strategy='afterInteractive'
-        />
-        <Script id='google-analytics' strategy='afterInteractive'>
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-0ESQQS3HTP');
-        `}
-        </Script>
       </Head>
       {children}
     </Box>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,49 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
 import { Footer } from '../components/Footer';
 import { Layout } from '../components/Layout';
 
+import { GTM_ID, pageview } from '../lib/analytics';
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Layout>
         <Box
           display='flex'
