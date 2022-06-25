82bbc969e6bcc11cb9a2df15f9b37b21c98a2d20
Robert Fuchs <robert.fuchs@futurice.com>
2022-06-25 12:52:57 +0200

    statically render song pages

--- pages/songs/[id].tsx
+++ pages/songs/[id].tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { Box } from '@chakra-ui/react';
 
 import { SongData, FeaturedSongs } from '../../lib/music-data';
@@ -8,7 +8,7 @@ import { Error } from '../../components/Error';
 import { BackButton } from '../../components/BackButton';
 
 export interface SongProps {
-  song: SongData | null;
+  song: SongData;
 }
 
 export default function Songs({ song }: SongProps) {
@@ -18,17 +18,26 @@ export default function Songs({ song }: SongProps) {
         <title>{song.title}</title>
         <meta name='description' content={`Lyrics für ${song.title}`} />
       </Head>
-      {song ? (
-        <Song {...song} marginBottom='2rem' />
-      ) : (
-        <Error message='Nichts gefunden 🤷' />
-      )}
+      <Song {...song} marginBottom='2rem' />
       <BackButton variant='solid' marginTop='2rem' />
     </Box>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = FeaturedSongs.map((song) => {
+    return {
+      params: { id: song.title.toLowerCase() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
   const { id } = context.params;
   const song = FeaturedSongs.find(
     (s) => s.title.toLowerCase() === (id as string).toLowerCase()
@@ -36,7 +45,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   return {
     props: {
-      song: song || null,
+      song: song,
     },
   };
 };
