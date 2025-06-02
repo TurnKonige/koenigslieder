d6cb533607df2bd9df0855491f686598c9e47402
Semjon Geist <semjon.geist@adesso.de>
2025-06-02 21:01:47 +0200

    add @contentful/rich-text-types

--- components/SongText.tsx
+++ components/SongText.tsx
@@ -1,39 +1,17 @@
 import { Box, Text } from '@chakra-ui/react';
-import { Document } from '@contentful/rich-text-types';
-import { useRichText } from '../hooks/useRichText';
 
 export interface SongTextProps {
   title: string;
-  lyrics: {
-    json: Document;
-    links: RichTextLinks;
-  };
-}
-
-export interface RichTextLinks {
-  entries: {
-    block: Refrain[];
-  };
-}
-
-interface Refrain {
-  typename: 'Refrain';
-  preview: string;
-  complete: string;
-  sys: {
-    id: string;
-  };
+  lyrics: string;
 }
 
 export const SongText: React.FC<SongTextProps> = ({ title, lyrics }) => {
-  const renderer = useRichText(lyrics.links);
-
   return (
     <Box>
-      <Text textAlign='center' as='h1' fontSize='2rem' mb='4'>
+      <Text fontSize="2xl" fontWeight="bold" mb={4}>
         {title}
       </Text>
-      {renderer.render(lyrics.json)}
+      <Text whiteSpace="pre-wrap">{lyrics}</Text>
     </Box>
   );
 };
